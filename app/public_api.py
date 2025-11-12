23f6de2eb9fea28eb96bcca77c0e46ed4c52fcc2
John Lei <leihan12345@gmail.com>
2025-11-12 12:47:05 -0500

    updating to add an api layer that exposes entries from MongoDB

---
+++ app/public_api.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+client = MongoClient(os.getenv("MONGODB_URI"))
+db = client["post_collection"]
+
+def serialize_doc(doc):
+    doc["_id"] = str(doc["_id"])
+    return doc
+
+@app.get("/posts/{subreddit}")
+def get_wallstreetbets_posts(subreddit:str):
+    collection = db[subreddit]
+    posts = list(collection.find().sort("created_utc", -1))
+    current_epoch_time = time.time()
+    one_week_window = current_epoch_time - 592200
+    display = []
+    for p in posts:
+        if p["created_utc"] < one_week_window:
+            break
+        display.append(p)
+    return [serialize_doc(d) for d in display]
